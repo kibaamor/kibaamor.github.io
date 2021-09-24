494baf726c6aa1caee13377c86d0fe193d924b6e
kibaamor <kibaamor@gmail.com>
2021-09-24 19:04:53 +0800

    Auto Deploy at 2021-09-24 19:04

---
+++ images/《动手学深度学习（第二版）》读书笔记/rnn.py
@@ -0,0 +1,343 @@
+import collections
+import hashlib
+import math
+import os
+import random
+import re
+
+import requests
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+DATA_HUB = dict()
+DATA_URL = "http://d2l-data.s3-accelerate.amazonaws.com/"
+DATA_HUB["time_machine"] = (DATA_URL + "timemachine.txt", "090b5e7e70c295757f55df93cb0a180b9691891a")
+
+
+def download(name, cache_dir=os.path.join(".", "data")):
+    """下载一个DATA_HUB中的文件，返回本地文件名。"""
+    assert name in DATA_HUB, f"{name} 不存在于 {DATA_HUB}."
+    url, sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir, exist_ok=True)
+    fname = os.path.join(cache_dir, url.split("/")[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, "rb") as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname  # Hit cache
+
+    print(f"正在从{url}下载{fname}...")
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, "wb") as f:
+        f.write(r.content)
+
+    return fname
+
+
+def read_time_machine():
+    """Load the time machine dataset into a list of text lines."""
+    with open(download("time_machine"), "r") as f:
+        lines = f.readlines()
+    return [re.sub("[^A-Za-z]+", " ", line).strip().lower() for line in lines]
+
+
+def tokenize(lines, token="word"):
+    """将文本行拆分为单词或字符词元。"""
+    if token == "word":
+        return [line.split() for line in lines]
+    elif token == "char":
+        return [list(line) for line in lines]
+    else:
+        print("错误：未知词元类型：" + token)
+
+
+class Vocab:
+    """文本词汇表"""
+
+    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        # 按出现频率排序
+        counter = count_corpus(tokens)
+        self.token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+        # 未知词元的索引为0
+        self.unk, uniq_tokens = 0, ["<unk>"] + reserved_tokens
+        uniq_tokens += [token for token, freq in self.token_freqs if freq >= min_freq and token not in uniq_tokens]
+        self.idx_to_token, self.token_to_idx = [], dict()
+        for token in uniq_tokens:
+            self.idx_to_token.append(token)
+            self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+
+def count_corpus(tokens):
+    """统计词元的频率。"""
+    # 这里的 `tokens` 是 1D 列表或 2D 列表
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        # 将词元列表展平成使用词元填充的一个列表
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+
+def load_corpus_time_machine(max_tokens=-1):
+    """返回时光机器数据集的词元索引列表和词汇表。"""
+    lines = read_time_machine()
+    tokens = tokenize(lines, "char")
+    vocab = Vocab(tokens)
+    # 因为时光机器数据集中的每个文本行不一定是一个句子或一个段落，
+    # 所以将所有文本行展平到一个列表中
+    corpus = [vocab[token] for line in tokens for token in line]
+    if max_tokens > 0:
+        corpus = corpus[:max_tokens]
+    return corpus, vocab
+
+
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    """使用随机抽样生成一个小批量子序列。"""
+    # 从随机偏移量开始对序列进行分区，随机范围包括`num_steps - 1`
+    corpus = corpus[random.randint(0, num_steps - 1) :]
+    # 减去1，是因为我们需要考虑标签
+    num_subseqs = (len(corpus) - 1) // num_steps
+    # 长度为`num_steps`的子序列的起始索引
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    # 在随机抽样的迭代过程中，
+    # 来自两个相邻的、随机的、小批量中的子序列不一定在原始序列上相邻
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        # 返回从`pos`位置开始的长度为`num_steps`的序列
+        return corpus[pos : pos + num_steps]
+
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        # 在这里，`initial_indices`包含子序列的随机起始索引
+        initial_indices_per_batch = initial_indices[i : i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 1) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+
+def seq_data_iter_sequential(corpus, batch_size, num_steps):  # @save
+    """使用顺序分区生成一个小批量子序列。"""
+    # 从随机偏移量开始划分序列
+    offset = random.randint(0, num_steps)
+    num_tokens = ((len(corpus) - offset - 1) // batch_size) * batch_size
+    Xs = torch.tensor(corpus[offset : offset + num_tokens])
+    Ys = torch.tensor(corpus[offset + 1 : offset + 1 + num_tokens])
+    Xs, Ys = Xs.reshape(batch_size, -1), Ys.reshape(batch_size, -1)
+    num_batches = Xs.shape[1] // num_steps
+    for i in range(0, num_steps * num_batches, num_steps):
+        X = Xs[:, i : i + num_steps]
+        Y = Ys[:, i : i + num_steps]
+        yield X, Y
+
+
+class SeqDataLoader:
+    """加载序列数据的迭代器。"""
+
+    def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
+        if use_random_iter:
+            self.data_iter_fn = seq_data_iter_random
+        else:
+            self.data_iter_fn = seq_data_iter_sequential
+        self.corpus, self.vocab = load_corpus_time_machine(max_tokens)
+        self.batch_size, self.num_steps = batch_size, num_steps
+
+    def __iter__(self):
+        return self.data_iter_fn(self.corpus, self.batch_size, self.num_steps)
+
+
+def load_data_time_machine(batch_size, num_steps, use_random_iter=False, max_tokens=10000):
+    """返回时光机器数据集的迭代器和词汇表。"""
+    data_iter = SeqDataLoader(batch_size, num_steps, use_random_iter, max_tokens)
+    return data_iter, data_iter.vocab
+
+
+def try_gpu(i=0):
+    """Return gpu(i) if exists, otherwise return cpu()."""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f"cuda:{i}")
+    return torch.device("cpu")
+
+
+def grad_clipping(net, theta):
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+
+class Accumulator:
+    """For accumulating sums over `n` variables."""
+
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def get_params(vocab_size, num_hiddens, device):
+    num_inputs = num_outputs = vocab_size
+
+    def normal(shape):
+        return torch.randn(size=shape, device=device) * 0.01
+
+    # 隐藏层参数
+    W_xh = normal((num_inputs, num_hiddens))
+    W_hh = normal((num_hiddens, num_hiddens))
+    b_h = torch.zeros(num_hiddens, device=device)
+    # 输出层参数
+    W_hq = normal((num_hiddens, num_outputs))
+    b_q = torch.zeros(num_outputs, device=device)
+    # 附加梯度
+    params = [W_xh, W_hh, b_h, W_hq, b_q]
+    for param in params:
+        param.requires_grad_(True)
+    return params
+
+
+def init_rnn_state(batch_size, num_hiddens, device):
+    return (torch.zeros((batch_size, num_hiddens), device=device),)
+
+
+def rnn(inputs, state, params):
+    # `inputs`的形状：(`时间步数量`，`批量大小`，`词表大小`)
+    W_xh, W_hh, b_h, W_hq, b_q = params
+    (H,) = state
+    outputs = []
+    # `X`的形状：(`批量大小`，`词表大小`)
+    for X in inputs:
+        H = torch.tanh(torch.mm(X, W_xh) + torch.mm(H, W_hh) + b_h)
+        Y = torch.mm(H, W_hq) + b_q
+        outputs.append(Y)
+    return torch.cat(outputs, dim=0), (H,)
+
+
+class RNNModelScratch:
+    """从零开始实现的循环神经网络模型"""
+
+    def __init__(self, vocab_size, num_hiddens, device, get_params, init_state, forward_fn):
+        self.vocab_size, self.num_hiddens = vocab_size, num_hiddens
+        self.params = get_params(vocab_size, num_hiddens, device)
+        self.init_state, self.forward_fn = init_state, forward_fn
+
+    def __call__(self, X, state):
+        X = F.one_hot(X.T, self.vocab_size).type(torch.float32)
+        return self.forward_fn(X, state, self.params)
+
+    def begin_state(self, batch_size, device):
+        return self.init_state(batch_size, self.num_hiddens, device)
+
+
+def predict_ch8(prefix, num_preds, net, vocab, device):
+    """在`prefix`后面生成新字符。"""
+    state = net.begin_state(batch_size=1, device=device)
+    outputs = [vocab[prefix[0]]]
+
+    def get_input():
+        return torch.tensor([outputs[-1]], device=device).reshape((1, 1))
+
+    for y in prefix[1:]:  # 预热期
+        _, state = net(get_input(), state)
+        outputs.append(vocab[y])
+
+    for _ in range(num_preds):  # 预测`num_preds`步
+        y, state = net(get_input(), state)
+        outputs.append(int(y.argmax(dim=1).reshape(1)))
+
+    return "".join([vocab.idx_to_token[i] for i in outputs])
+
+
+def train_epoch_ch8(net, train_iter, loss, updater, device, use_random_iter):
+    """训练模型一个迭代周期（定义见第8章）。"""
+    state = None
+    metrics = [0.0, 0.0]  # 训练损失之和, 词元数量
+    for X, Y in train_iter:
+        if state is None or use_random_iter:
+            # 在第一次迭代或使用随机抽样时初始化`state`
+            state = net.begin_state(batch_size=X.shape[0], device=device)
+        else:
+            # `state`我们从零开始实现的模型是个张量
+            for s in state:
+                s.detach_()
+
+        y = Y.T.reshape(-1)
+        X, y = X.to(device), y.to(device)
+        y_hat, state = net(X, state)
+
+        l = loss(y_hat, y.long()).mean()
+        l.backward()
+        grad_clipping(net, 1)
+        # 因为已经调用了`mean`函数
+        updater(batch_size=1)
+
+        metrics[0] = metrics[0] + l * y.numel()
+        metrics[1] = metrics[1] + y.numel()
+    return math.exp(metrics[0] / metrics[1])  # 困惑度
+
+
+def sgd(params, lr, batch_size):
+    """Minibatch stochastic gradient descent."""
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
+
+def train_ch8(net, train_iter, vocab, lr, num_epochs, device, use_random_iter=False):
+    """训练模型（定义见第8章）。"""
+    loss = nn.CrossEntropyLoss()
+
+    # 初始化
+    updater = lambda batch_size: sgd(net.params, lr, batch_size)
+    predict = lambda prefix: predict_ch8(prefix, 50, net, vocab, device)
+
+    # 训练和预测
+    for epoch in range(num_epochs):
+        ppl = train_epoch_ch8(net, train_iter, loss, updater, device, use_random_iter)
+        if (epoch + 1) % 10 == 0:
+            print(predict("time traveller"))
+            print(f"困惑度 {ppl:.1f}")
+
+    print(predict("time traveller"))
+    print(predict("traveller"))
+
+
+batch_size, num_steps = 32, 35
+num_hiddens = 512
+train_iter, vocab = load_data_time_machine(batch_size, num_steps)
+net = RNNModelScratch(len(vocab), num_hiddens, try_gpu(), get_params, init_rnn_state, rnn)
+
+num_epochs, lr = 500, 1
+train_ch8(net, train_iter, vocab, lr, num_epochs, try_gpu())
