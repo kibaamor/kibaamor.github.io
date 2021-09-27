06243b01ba89737f5c00f53dbc57d4e59f8de132
kibaamor <kibaamor@gmail.com>
2021-09-27 16:27:33 +0800

    Auto Deploy at 2021-09-27 16:27

---
+++ images/《动手学深度学习（第二版）》读书笔记/machine_translation_with_bahdanau_attention.py
@@ -0,0 +1,482 @@
+import collections
+import hashlib
+import math
+import os
+import tarfile
+import zipfile
+
+import requests
+import torch
+from torch import nn
+from torch.utils import data
+
+DATA_HUB = dict()
+DATA_URL = "http://d2l-data.s3-accelerate.amazonaws.com/"
+DATA_HUB["fra-eng"] = (DATA_URL + "fra-eng.zip", "94646ad1522d915e7b0f9296181140edcf86a4f5")
+
+
+def download(name, cache_dir=os.path.join(".", "data")):
+    """下载一个DATA_HUB中的文件，返回本地文件名。"""
+    assert name in DATA_HUB, f"{name} 不存在于 {DATA_HUB}."
+    url, sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir, exist_ok=True)
+    fname = os.path.join(cache_dir, url.split("/")[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, "rb") as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname  # Hit cache
+
+    print(f"正在从{url}下载{fname}...")
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, "wb") as f:
+        f.write(r.content)
+
+    return fname
+
+
+def download_extract(name, folder=None):
+    """下载并解压zip/tar文件。"""
+    fname = download(name)
+    base_dir = os.path.dirname(fname)
+    data_dir, ext = os.path.splitext(fname)
+    if ext == ".zip":
+        fp = zipfile.ZipFile(fname, "r")
+    elif ext in (".tar", ".gz"):
+        fp = tarfile.open(fname, "r")
+    else:
+        assert False, "只有zip/tar文件可以被解压缩。"
+    fp.extractall(base_dir)
+    return os.path.join(base_dir, folder) if folder else data_dir
+
+
+def read_data_nmt():
+    """载入“英语－法语”数据集。"""
+    data_dir = download_extract("fra-eng")
+    with open(os.path.join(data_dir, "fra.txt"), "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def preprocess_nmt(text):
+    """预处理“英语－法语”数据集。"""
+
+    def no_space(char, prev_char):
+        return char in set(",.!?") and prev_char != " "
+
+    # 使用空格替换不间断空格
+    # 使用小写字母替换大写字母
+    text = text.replace("\u202f", " ").replace("\xa0", " ").lower()
+    # 在单词和标点符号之间插入空格
+    out = [" " + char if i > 0 and no_space(char, text[i - 1]) else char for i, char in enumerate(text)]
+    return "".join(out)
+
+
+def tokenize_nmt(text, num_examples=None):
+    """词元化“英语－法语”数据数据集。"""
+    source, target = [], []
+    for i, line in enumerate(text.split("\n")):
+        if num_examples and i > num_examples:
+            break
+        parts = line.split("\t")
+        if len(parts) == 2:
+            source.append(parts[0].split(" "))
+            target.append(parts[1].split(" "))
+    return source, target
+
+
+def count_corpus(tokens):
+    """统计词元的频率。"""
+    # 这里的 `tokens` 是 1D 列表或 2D 列表
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        # 将词元列表展平成使用词元填充的一个列表
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+
+class Vocab:
+    """文本词汇表"""
+
+    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        # 按出现频率排序
+        counter = count_corpus(tokens)
+        self.token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+        # 未知词元的索引为0
+        self.unk, uniq_tokens = 0, ["<unk>"] + reserved_tokens
+        uniq_tokens += [token for token, freq in self.token_freqs if freq >= min_freq and token not in uniq_tokens]
+        self.idx_to_token, self.token_to_idx = [], dict()
+        for token in uniq_tokens:
+            self.idx_to_token.append(token)
+            self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+
+def truncate_pad(line, num_steps, padding_token):
+    """截断或填充文本序列。"""
+    if len(line) > num_steps:
+        return line[:num_steps]  # 截断
+    return line + [padding_token] * (num_steps - len(line))  # 填充
+
+
+def build_array_nmt(lines, vocab, num_steps):
+    """将机器翻译的文本序列转换成小批量。"""
+    lines = [vocab[l] for l in lines]
+    lines = [l + [vocab["<eos>"]] for l in lines]
+    array = torch.tensor([truncate_pad(l, num_steps, vocab["<pad>"]) for l in lines])
+    valid_len = (array != vocab["<pad>"]).type(torch.int32).sum(1)
+    return array, valid_len
+
+
+def load_array(data_arrays, batch_size, is_train=True):
+    """Construct a PyTorch data iterator."""
+    dataset = data.TensorDataset(*data_arrays)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+
+def load_data_nmt(batch_size, num_steps, num_examples=600):
+    """返回翻译数据集的迭代器和词汇表。"""
+    text = preprocess_nmt(read_data_nmt())
+    source, target = tokenize_nmt(text, num_examples)
+    src_vocab = Vocab(source, min_freq=2, reserved_tokens=["<pad>", "<bos>", "<eos>"])
+    tgt_vocab = Vocab(target, min_freq=2, reserved_tokens=["<pad>", "<bos>", "<eos>"])
+    src_array, src_valid_len = build_array_nmt(source, src_vocab, num_steps)
+    tgt_array, tgt_valid_len = build_array_nmt(target, tgt_vocab, num_steps)
+    data_arrays = (src_array, src_valid_len, tgt_array, tgt_valid_len)
+    data_iter = load_array(data_arrays, batch_size)
+    return data_iter, src_vocab, tgt_vocab
+
+
+class Encoder(nn.Module):
+    """编码器-解码器结构的基本编码器接口。"""
+
+    def __init__(self, **kwargs):
+        super(Encoder, self).__init__(**kwargs)
+
+    def forward(self, X, *args):
+        raise NotImplementedError
+
+
+class Decoder(nn.Module):
+    """编码器-解码器结构的基本解码器接口。"""
+
+    def __init__(self, **kwargs):
+        super(Decoder, self).__init__(**kwargs)
+
+    def init_state(self, enc_outputs, *args):
+        raise NotImplementedError
+
+    def forward(self, X, state):
+        raise NotImplementedError
+
+
+class EncoderDecoder(nn.Module):
+    """编码器-解码器结构的基类。"""
+
+    def __init__(self, encoder, decoder, **kwargs):
+        super(EncoderDecoder, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, enc_X, dec_X, *args):
+        enc_outputs = self.encoder(enc_X, *args)
+        dec_state = self.decoder.init_state(enc_outputs, *args)
+        return self.decoder(dec_X, dec_state)
+
+
+class AttentionDecoder(Decoder):
+    """带有注意力机制的解码器基本接口"""
+
+    def __init__(self, **kwargs):
+        super(AttentionDecoder, self).__init__(**kwargs)
+
+    @property
+    def attention_weights(self):
+        raise NotImplementedError
+
+
+class Seq2SeqEncoder(Encoder):
+    """用于序列到序列学习的循环神经网络编码器。"""
+
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0, **kwargs):
+        super(Seq2SeqEncoder, self).__init__(**kwargs)
+        # 嵌入层
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, dropout=dropout)
+
+    def forward(self, X, *args):
+        # 输出'X'的形状：(`batch_size`, `num_steps`, `embed_size`)
+        X = self.embedding(X)
+        # 在循环神经网络模型中，第一个轴对应于时间步
+        X = X.permute(1, 0, 2)
+        # 如果未提及状态，则默认为0
+        output, state = self.rnn(X)
+        # `output`的形状: (`num_steps`, `batch_size`, `num_hiddens`)
+        # `state[0]`的形状: (`num_layers`, `batch_size`, `num_hiddens`)
+        return output, state
+
+
+class Seq2SeqDecoder(Decoder):
+    """用于序列到序列学习的循环神经网络解码器。"""
+
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0, **kwargs):
+        super(Seq2SeqDecoder, self).__init__(**kwargs)
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size + num_hiddens, num_hiddens, num_layers, dropout=dropout)
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, *args):
+        return enc_outputs[1]
+
+    def forward(self, X, state):
+        # 输出'X'的形状：(`batch_size`, `num_steps`, `embed_size`)
+        X = self.embedding(X).permute(1, 0, 2)
+        # 广播`context`，使其具有与`X`相同的`num_steps`
+        context = state[-1].repeat(X.shape[0], 1, 1)
+        X_and_context = torch.cat((X, context), 2)
+        output, state = self.rnn(X_and_context, state)
+        output = self.dense(output).permute(1, 0, 2)
+        # `output`的形状: (`batch_size`, `num_steps`, `vocab_size`)
+        # `state[0]`的形状: (`num_layers`, `batch_size`, `num_hiddens`)
+        return output, state
+
+
+class Seq2SeqAttentionDecoder(AttentionDecoder):
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0, **kwargs):
+        super(Seq2SeqAttentionDecoder, self).__init__(**kwargs)
+        self.attention = AdditiveAttention(num_hiddens, num_hiddens, num_hiddens, dropout)
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size + num_hiddens, num_hiddens, num_layers, dropout=dropout)
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, enc_valid_lens, *args):
+        # `enc_outputs`的形状为 (`batch_size`, `num_steps`, `num_hiddens`).
+        # `hidden_state`的形状为 (`num_layers`, `batch_size`, `num_hiddens`)
+        outputs, hidden_state = enc_outputs
+        return (outputs.permute(1, 0, 2), hidden_state, enc_valid_lens)
+
+    def forward(self, X, state):
+        # `enc_outputs`的形状为 (`batch_size`, `num_steps`, `num_hiddens`).
+        # `hidden_state`的形状为 (`num_layers`, `batch_size`, `num_hiddens`)
+        enc_outputs, hidden_state, enc_valid_lens = state
+        # 输出 `X`的形状为 (`num_steps`, `batch_size`, `embed_size`)
+        X = self.embedding(X).permute(1, 0, 2)
+        outputs, self._attention_weights = [], []
+        for x in X:
+            # `query`的形状为 (`batch_size`, 1, `num_hiddens`)
+            query = torch.unsqueeze(hidden_state[-1], dim=1)
+            # `context`的形状为 (`batch_size`, 1, `num_hiddens`)
+            context = self.attention(query, enc_outputs, enc_outputs, enc_valid_lens)
+            # 在特征维度上连结
+            x = torch.cat((context, torch.unsqueeze(x, dim=1)), dim=-1)
+            # 将 `x` 变形为 (1, `batch_size`, `embed_size` + `num_hiddens`)
+            out, hidden_state = self.rnn(x.permute(1, 0, 2), hidden_state)
+            outputs.append(out)
+            self._attention_weights.append(self.attention.attention_weights)
+        # 全连接层变换后， `outputs`的形状为
+        # (`num_steps`, `batch_size`, `vocab_size`)
+        outputs = self.dense(torch.cat(outputs, dim=0))
+        return outputs.permute(1, 0, 2), [enc_outputs, hidden_state, enc_valid_lens]
+
+    @property
+    def attention_weights(self):
+        return self._attention_weights
+
+
+def sequence_mask(X, valid_len, value=0):
+    """在序列中屏蔽不相关的项。"""
+    maxlen = X.size(1)
+    mask = torch.arange((maxlen), dtype=torch.float32, device=X.device)[None, :] < valid_len[:, None]
+    X[~mask] = value
+    return X
+
+
+def masked_softmax(X, valid_lens):
+    """通过在最后一个轴上遮蔽元素来执行 softmax 操作"""
+    # `X`: 3D张量, `valid_lens`: 1D或2D 张量
+    if valid_lens is None:
+        return nn.functional.softmax(X, dim=-1)
+    else:
+        shape = X.shape
+        if valid_lens.dim() == 1:
+            valid_lens = torch.repeat_interleave(valid_lens, shape[1])
+        else:
+            valid_lens = valid_lens.reshape(-1)
+        # 在最后的轴上，被遮蔽的元素使用一个非常大的负值替换，从而其 softmax (指数)输出为 0
+        X = sequence_mask(X.reshape(-1, shape[-1]), valid_lens, value=-1e6)
+        return nn.functional.softmax(X.reshape(shape), dim=-1)
+
+
+class AdditiveAttention(nn.Module):
+    """加性注意力"""
+
+    def __init__(self, key_size, query_size, num_hiddens, dropout, **kwargs):
+        super(AdditiveAttention, self).__init__(**kwargs)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=False)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=False)
+        self.w_v = nn.Linear(num_hiddens, 1, bias=False)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, queries, keys, values, valid_lens):
+        queries, keys = self.W_q(queries), self.W_k(keys)
+        # 在维度扩展后，
+        # `queries` 的形状：(`batch_size`, 查询的个数, 1, `num_hidden`)
+        # `key` 的形状：(`batch_size`, 1, “键－值”对的个数, `num_hiddens`)
+        # 使用广播方式进行求和
+        features = queries.unsqueeze(2) + keys.unsqueeze(1)
+        features = torch.tanh(features)
+        # `self.w_v` 仅有一个输出，因此从形状中移除最后那个维度。
+        # `scores` 的形状：(`batch_size`, 查询的个数, “键-值”对的个数)
+        scores = self.w_v(features).squeeze(-1)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        # `values` 的形状：(`batch_size`, “键－值”对的个数, 值的维度)
+        return torch.bmm(self.dropout(self.attention_weights), values)
+
+
+class MaskedSoftmaxCELoss(nn.CrossEntropyLoss):
+    """带遮蔽的softmax交叉熵损失函数"""
+
+    # `pred` 的形状：(`batch_size`, `num_steps`, `vocab_size`)
+    # `label` 的形状：(`batch_size`, `num_steps`)
+    # `valid_len` 的形状：(`batch_size`,)
+    def forward(self, pred, label, valid_len):
+        weights = torch.ones_like(label)
+        weights = sequence_mask(weights, valid_len)
+        self.reduction = "none"
+        unweighted_loss = super(MaskedSoftmaxCELoss, self).forward(pred.permute(0, 2, 1), label)
+        weighted_loss = (unweighted_loss * weights).mean(dim=1)
+        return weighted_loss
+
+
+def grad_clipping(net, theta):
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+
+def train_seq2seq(net, data_iter, lr, num_epochs, tgt_vocab, device):
+    """训练序列到序列模型。"""
+
+    def xavier_init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.xavier_uniform_(m.weight)
+        if type(m) == nn.GRU:
+            for param in m._flat_weights_names:
+                if "weight" in param:
+                    nn.init.xavier_uniform_(m._parameters[param])
+
+    net.apply(xavier_init_weights)
+    net.to(device)
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    loss = MaskedSoftmaxCELoss()
+    net.train()
+    for epoch in range(num_epochs):
+        metrics = [0.0, 0.0]
+        for batch in data_iter:
+            X, X_valid_len, Y, Y_valid_len = [x.to(device) for x in batch]
+            bos = torch.tensor([tgt_vocab["<bos>"]] * Y.shape[0], device=device).reshape(-1, 1)
+            dec_input = torch.cat([bos, Y[:, :-1]], 1)  # 教师强制
+            Y_hat, _ = net(X, dec_input, X_valid_len)
+            l = loss(Y_hat, Y, Y_valid_len)
+            l.sum().backward()  # 损失函数的标量进行“反传”
+            grad_clipping(net, 1)
+            num_tokens = Y_valid_len.sum()
+            optimizer.step()
+            with torch.no_grad():
+                metrics[0] = metrics[0] + l.sum()
+                metrics[1] = metrics[1] + num_tokens
+            print(f"loss {metrics[0] / metrics[1]:.3f}")
+
+
+def try_gpu(i=0):
+    """Return gpu(i) if exists, otherwise return cpu()."""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f"cuda:{i}")
+    return torch.device("cpu")
+
+
+def predict_seq2seq(net, src_sentence, src_vocab, tgt_vocab, num_steps, device, save_attention_weights=False):
+    """序列到序列模型的预测"""
+    # 在预测时将`net`设置为评估模式
+    net.eval()
+    src_tokens = src_vocab[src_sentence.lower().split(" ")] + [src_vocab["<eos>"]]
+    enc_valid_len = torch.tensor([len(src_tokens)], device=device)
+    src_tokens = truncate_pad(src_tokens, num_steps, src_vocab["<pad>"])
+    # 添加批量轴
+    enc_X = torch.unsqueeze(torch.tensor(src_tokens, dtype=torch.long, device=device), dim=0)
+    enc_outputs = net.encoder(enc_X, enc_valid_len)
+    dec_state = net.decoder.init_state(enc_outputs, enc_valid_len)
+    # 添加批量轴
+    dec_X = torch.unsqueeze(torch.tensor([tgt_vocab["<bos>"]], dtype=torch.long, device=device), dim=0)
+    output_seq, attention_weight_seq = [], []
+    for _ in range(num_steps):
+        Y, dec_state = net.decoder(dec_X, dec_state)
+        # 我们使用具有预测最高可能性的词元，作为解码器在下一时间步的输入
+        dec_X = Y.argmax(dim=2)
+        pred = dec_X.squeeze(dim=0).type(torch.int32).item()
+        # 保存注意力权重（稍后讨论）
+        if save_attention_weights:
+            attention_weight_seq.append(net.decoder.attention_weights)
+        # 一旦序列结束词元被预测，输出序列的生成就完成了
+        if pred == tgt_vocab["<eos>"]:
+            break
+        output_seq.append(pred)
+    return " ".join(tgt_vocab.to_tokens(output_seq)), attention_weight_seq
+
+
+def bleu(pred_seq, label_seq, k):  # @save
+    """计算 BLEU"""
+    pred_tokens, label_tokens = pred_seq.split(" "), label_seq.split(" ")
+    len_pred, len_label = len(pred_tokens), len(label_tokens)
+    score = math.exp(min(0, 1 - len_label / len_pred))
+    for n in range(1, k + 1):
+        num_matches, label_subs = 0, collections.defaultdict(int)
+        for i in range(len_label - n + 1):
+            label_subs["".join(label_tokens[i : i + n])] += 1
+        for i in range(len_pred - n + 1):
+            if label_subs["".join(pred_tokens[i : i + n])] > 0:
+                num_matches += 1
+                label_subs["".join(pred_tokens[i : i + n])] -= 1
+        score *= math.pow(num_matches / (len_pred - n + 1), math.pow(0.5, n))
+    return score
+
+
+embed_size, num_hiddens, num_layers, dropout = 32, 32, 2, 0.1
+batch_size, num_steps = 64, 10
+lr, num_epochs, device = 0.005, 250, try_gpu()
+
+train_iter, src_vocab, tgt_vocab = load_data_nmt(batch_size, num_steps)
+encoder = Seq2SeqEncoder(len(src_vocab), embed_size, num_hiddens, num_layers, dropout)
+decoder = Seq2SeqAttentionDecoder(len(tgt_vocab), embed_size, num_hiddens, num_layers, dropout)
+net = EncoderDecoder(encoder, decoder)
+train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
+
+engs = ["go .", "i lost .", "he's calm .", "i'm home ."]
+fras = ["va !", "j'ai perdu .", "il est calme .", "je suis chez moi ."]
+for eng, fra in zip(engs, fras):
+    translation, attention_weight_seq = predict_seq2seq(net, eng, src_vocab, tgt_vocab, num_steps, device)
+    print(f"{eng} => {translation}, bleu {bleu(translation, fra, k=2):.3f}")
